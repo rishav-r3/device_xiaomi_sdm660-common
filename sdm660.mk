717ee40ebfa7bc831001915033e9bbe45094792d
Rahul <rahul731999@gmail.com>
2021-10-14 06:27:26 +0530

    Revert "whyred: add display config file"
    This reverts commit 48d5f3e26438666282cb83486a0322eaf7de2b66.
    
    Signed-off-by: arnavpuranik <puranikarnav@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     vendor.display.config@1.1
 
-# Display Device Config
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
-
 # DPM
 PRODUCT_PACKAGES += \
     libshim_dpmframework
