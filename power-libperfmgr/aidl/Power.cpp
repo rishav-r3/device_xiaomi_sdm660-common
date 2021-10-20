6f5eab8eef336430e786854d45906215cdfab2e3
Arnav Puranik <puranikarnav@gmail.com>
2021-10-20 20:30:04 +0530

    sdm660-common: power-libperfmgr: Switch DT2W Node
    * Switch DT2W Node to /sys/touchpanel/double_tap to fix DT2W on my kernel

--- power-libperfmgr/aidl/Power.cpp
+++ power-libperfmgr/aidl/Power.cpp
@@ -30,7 +30,7 @@
 #include <utils/Trace.h>
 
 #ifndef TAP_TO_WAKE_NODE
-#define TAP_TO_WAKE_NODE "/proc/tp_gesture"
+#define TAP_TO_WAKE_NODE "/sys/touchpanel/double_tap"
 #endif
 
 namespace aidl {
